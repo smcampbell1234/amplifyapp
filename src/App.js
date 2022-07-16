a64e666c4edc1662930aab2b70f3f435ed6a4afd
Sean Campbell <writer.sandiego@gmail.com>
2022-07-15 22:47:33 -0700

    test CI/CD

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Test CI/CD on AWS Amplify</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
